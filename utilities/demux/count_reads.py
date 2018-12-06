ed31949b8d7468584ef262388434d21e42ffa039
David Stone <dstone@tracegenomics.com>
2018-12-06 05:22:18 +0000

    Updates to the count_reads script

--- utilities/demux/count_reads.py
+++ utilities/demux/count_reads.py
@@ -1,6 +1,10 @@
 import argparse
+import logging
+import os
+import re
 
 import boto3
+import pandas as pd
 
 
 def arguments():
@@ -11,17 +15,40 @@ def arguments():
 			required=True)
     parser.add_argument('-bucket',
                         type=str,
-                        help='Name of S3 bucket to read from.',
-                        default='trace-genomics')
+                        help='Name of S3 bucket to read from.\n\tExample: tg-demux-sequence-data-prod',
+                        default='tg-demux-sequence-data-prod')
+    parser.add_argument('-lab',
+                        type=str,
+                        help='Name of S3 prefix to where a prefix with Run{run} should exist with fastq '
+                        'files.\n\tExample: TraceGenomics\n\t\t\t will lead to s3://<bucket>/TraceGenomics/Run{run}/',
+                        default='TraceGenomics')
     parser.add_argument('-logname',
                         type=str,
                         help='Name of logfile to use.',
                         default=None)
+    parser.add_argument('-verbose',
+                        action='store_true',
+                        help='Use this arg if you want to see intermediate and detailed outputs.')
     args = parser.parse_args()
     return args, parser
 
 
-def _get_total_reads_for_run(run, s3_bucket='trace-genomics'):
+def sort_for_humans(input_list):
+    """
+    Sort the given list in the way that humans expect.
+    Adapted from https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/
+
+    E.g. ['Field 1', 'field 10', 'Field 12', 'field 13', 'Field 2', 'field 23']
+    will produce (note case-insensitivity)
+         ['Field 1', 'Field 2', 'field 10', 'Field 12', 'field 13', 'field 23']
+    """
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(input_list, key=alphanum_key)
+
+
+def _get_total_reads_for_run(run, s3_bucket, s3_prefix):
+    logger = baselog.getChild('_get_total_reads_for_run')
     # should probably abstract this at some point and do something like du.get_database_connection() with boto clients to enforce singleton patterns
     s3 = boto3.client('s3')
 
@@ -29,11 +56,12 @@ def _get_total_reads_for_run(run, s3_bucket='trace-genomics'):
     # don't duplicate read counts, only count reads from forward fastq file
     _FASTQ_FORWARD_REGEX = re.compile('^(?P<run_id>Run\d+_\d+)_S\d+_R1_001.fastq.gz$')
 
-    s3lo = s3.list_objects(Bucket=s3_bucket, Prefix='sequencing_data/TraceGenomics/Run{}/'.format(run))
+    s3lo = s3.list_objects(Bucket=s3_bucket, Prefix='{}/Run{}/'.format(s3_prefix, run))
     for o in s3lo['Contents']:
         m = _FASTQ_FORWARD_REGEX.match(o['Key'].split('/')[-1])
         if m:
             run_id = m.group('run_id')
+            logger.info('Found forward fastq read for {}. Counting no. reads...'.format(run_id))
             r = s3.select_object_content(
                 Bucket=s3_bucket,
                 Key=o['Key'],
@@ -53,12 +81,13 @@ def _get_total_reads_for_run(run, s3_bucket='trace-genomics'):
             if len(total_reads) != 1:
                 raise ValueError('Expected only one measurement of total lines in fastq file, instead got {} possible values return from boto EventStream'.format(len(total_lines)))
             total_reads = total_reads[0]
+            logger.info('Found {} total reads for {} fastq file.'.format(total_reads, run_id))
             reads_dict[run_id] = total_reads
     return reads_dict
 
 
-def _output_reads_for_run_to_csv(run, output_file=None, **kwargs):
-    reads_dict = _get_total_reads_for_run(run, **kwargs)
+def _output_reads_for_run_to_csv(reads_dict, output_file=None,):
+    logger = baselog.getChild('_output_reads_for_run_to_csv')
     # use pandas as a convenience to write to csv for us
     # clean up and sort the run IDs in the 'natural' way with sort_for_humans
     df_reads = pd.DataFrame.from_dict(reads_dict, orient='index')
@@ -66,11 +95,17 @@ def _output_reads_for_run_to_csv(run, output_file=None, **kwargs):
 
     if output_file is None:
         output_file = './run{}_reads.csv'.format(run)
+    logger.info('Reads information is being output to the following file: {}'.format(output_file))
     df_reads.to_csv(output_file, header=None)
 
 
 def main(args):
-    _output_reads_for_run_to_csv(args.run)
+    logger = baselog.getChild('main')
+    logger.info('Generating dictionary of reads per run ID...')
+    reads_dict = _get_total_reads_for_run(args.run, args.bucket, args.lab)
+    logger.info('Outputting result to csv...')
+    _output_reads_for_run_to_csv(reads_dict)
+    logger.info('Done!')
 
 
 if __name__ == '__main__':
@@ -82,6 +117,10 @@ if __name__ == '__main__':
         logname = args.logname
     logname = os.path.abspath(logname)
 
+    if args.verbose:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
     # goes to the console and will ignore further basicConfig calls. Remove the handler if there is one.
     root = logging.getLogger()
     if root.handlers:
